b03cc727d6a9d11f9d4e40a220280b2de3fa397d
i-am-ethan <izawaryosuke0101@gmail.com>
2025-11-30 23:26:11 +0900

    lexer内で文字を識別する

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,12 +1,23 @@
 package lexer
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func Lexer(input string) {
 	fmt.Printf("lexer input: %s\n", input)
 
 	for i, ch := range input {
-		fmt.Printf("position: %d, ch: %c\n", i, ch)
+		// fmt.Printf("position: %d, ch: %c\n", i, ch)
+		switch {
+		case unicode.IsDigit(ch):
+			fmt.Printf("position: %d, %c is digit\n", i, ch)
+		case ch == '+':
+			fmt.Printf("position: %d, %c is plus\n", i, ch)
+		default:
+			fmt.Printf("position: %d, %c is other\n", i, ch)
+		}
 	}
 }
 
